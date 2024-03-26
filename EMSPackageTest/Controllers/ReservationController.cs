e1b5e38d99a1b827cccbb72c6e07643239678938
Dz0nZ1 <dzonzi777@gmail.com>
2024-03-26 11:47:30 +0100

    Created EMSPackageTest

---
+++ EMSPackageTest/Controllers/ReservationController.cs
@@ -0,0 +1,32 @@
+using EMSPackage.Application.Client;
+using EMSPackage.Application.Models;
+using EMSPackage.Sdk.Dto;
+using EMSPackage.Sdk.Dto.Reservation;
+using Microsoft.AspNetCore.Mvc;
+using Refit;
+
+namespace EMSPackageTest.Controllers;
+
+public class ReservationController : ControllerBase
+{
+    [HttpPost("create")]
+    public async Task<IActionResult> Create(EmsCreateReservationDto reservation)
+    {
+        var httpClient = new HttpClient
+        {
+            BaseAddress = new Uri("http://localhost:5191"),
+        };
+
+        var api = RestService.For<IEmsApi>(httpClient);
+        var client = new EmsSdkClient(api);
+        
+        var headers = new Dictionary<string, string> 
+        {
+            {"X-De-Username", "nikola"},
+            {"X-De-Password", "123"}
+        };
+        
+        var result = await client.CreateReservationAsync(new EmsReservationRequestModel(reservation.ReservationDate, reservation.Price, reservation.hasCupon, reservation.EventId, reservation.UserId, "http://localhost:5191", headers));
+        return Ok(result);
+    }
+}
